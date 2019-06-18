a3bc7a350b234a539bc74b326df0599a130d901e
LFMB <lfmblinux@LFMBComp.localdomain>
2019-06-18 09:09:29 -0400

    starting to work on cleaning up the build process

--- restaurant-mgmt-system/src/index.js
+++ restaurant-mgmt-system/src/index.js
@@ -2,3 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import reducer from "./reducers/reducer.js";
+import addTableItem from "./actions/addTableItem.js";
+import deleteTableItem from "./actions/deleteTableItem.js";
+import incrementMoneyEarned from "./actions/incrementMoneyEarned.js";
+import selectTable from "./actions/selectTable.js";
+import toggleTable from "./actions/toggleTable.js";
+
+// need to create styles.css and import it
+
+var store = createStore(reducer);
+
+
+function App() {
+  return (
+    <div className="App">
+		<h1>Hello</h1>
+	</div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
